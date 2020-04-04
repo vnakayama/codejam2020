a5095adc61ccc32c8fb5455bddfeef211b7cf482
Valentina Nakayama <v_nakayama@outlook.com>
2020-04-04 02:06:26 -0300

    first try

--- vestigium.py
+++ vestigium.py
@@ -7,3 +7,15 @@ r linhas com repetição
 c colunas com repetição
 """
 
+
+
+for t in range(1,T+1):
+	k = 0
+	r = 0
+	c = 0
+	for n in range(1,N+1):
+		for m in range(1,N+1):
+			if len(M[m]) != len(set(M[m])):
+				r = r +1
+			k = k + M[m-1]
+	print('Case #{}: {} {} {}'.format(t,k,r,c))
